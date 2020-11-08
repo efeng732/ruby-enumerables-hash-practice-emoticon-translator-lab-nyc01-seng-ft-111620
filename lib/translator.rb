03d7b70212a573a894ec33583172a18479568984
Erwin Feng <efeng732@gmail.com>
2020-11-08 19:06:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,33 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+def load_library(file)
   # code goes here
+  my_hash = {"emotion" => {:english => {}, :japanese => {}}}
+  library = YAML.load_file("./lib/emoticons.yml")
+  
+  library.each do |emotion, array|
+    print array
+    my_hash[emotion] = {:english => array[0], :japanese => array[1]}
+  end 
+my_hash   
 end
 
 def get_japanese_emoticon
   # code goes here
 end
 
-def get_english_meaning
+def get_english_meaning(file, emotion)
   # code goes here
+  suffering = load_library(file)
+  
+  suffering.each do |element|
+    emoji = element[emotion]
+    if emoji[1] == emotion  
+      return meaning 
+    end 
+  end 
+    
+  
 end
